bb33e26f5541623bd44f7c3a3448de5951e59ad6
Pavlo Pustovietov <pashkapustik@gmail.com>
2020-04-07 00:03:40 +0300

    Add some views

---
+++ ProductService/Controllers/MainController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult MainWindow()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProductService/Controllers/ProductFindController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.Controllers
+{
+    public class ProductFindController : Controller
+    {
+        public IActionResult ProductFindWindow()
+        {
+            return View();
+        }
+    }
+}
